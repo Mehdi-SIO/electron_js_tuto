59636bba3bf212081f31b996299e1c27053aa967
Mehdi OUAGHLANI <mouaghlani@octo.com>
2016-12-25 15:06:15 +0100

    Updated main and home

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 const electron = require('electron');
 const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const BrowserWindow = electron.BrowserWindow
+const ipc = electron.ipcMain;
 
 let mainWindow;
 
 function createWindow () {
 
-    mainWindow = new BrowserWindow({width: 1800, height: 1200});
+    mainWindow = new BrowserWindow({
+        width: 1800,
+        height: 1200,
+        movable: false,
+        title: "Hands on Electron",
+        frame: false,
+        maximized: false,
+        center: true
+    });
 
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 
     mainWindow.on('closed', () => {
         mainWindow = null;
     })
+
+    const childWindow = new BrowserWindow({
+       parent: mainWindow
+    });
 }
 
 app.on('ready', createWindow);
@@ -27,4 +40,8 @@ app.on('activate', () => {
     if (mainWindow === null) {
         createWindow();
     }
+});
+
+ipc.on('log-error', () => {
+    console.log('Erreur ! Veuillez rapporter ce bug au développeur de l\'application.');
 });
